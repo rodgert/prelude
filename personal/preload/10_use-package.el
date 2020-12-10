53dca655fb655a32944eaffc02293215cdfe445b
Thomas Rodgers <rodgert@twrodgers.com>
2020-12-09 18:50:43 -0800

    Checkpoint - A decent working configuration

--- personal/preload/10_use-package.el
+++ personal/preload/10_use-package.el
@@ -1,11 +1,3 @@
-;; use-package setup
-;; (prelude-require-package 'package)
-;; (setq package-enable-at-startup nil)
-;; (add-to-list 'package-archives'("melpa" . "https://melpa.org/packages/"))
-;; add-to-list 'package-archives'("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
-;; (package-initialize)
-;; bootstrap use-package
-
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
--- personal/preload/20_appearance.el
+++ personal/preload/20_appearance.el
@@ -4,3 +4,6 @@
   (setq prelude-theme 'gruvbox-dark-medium))
 
 (menu-bar-mode -1)
+
+(add-to-list 'default-frame-alist
+             '(font . "Source Code Pro-16"))
